b13e61100718c2db943f99dcc1ee50ee893bddf9
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-09-20 01:03:17 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
+          <a
+            className="App-link"
+            href="https://github.com/AlaaShaheen2/Math-Magicians"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Alaa Project
+          </a>
         </p>
         <a
           className="App-link"
