13db4891d18e8430a15ae9dba15ad4cebcd7f1ef
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-09-20 23:55:25 +0300

    Delete all the boilerplate

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <a
-            className="App-link"
-            href="https://github.com/AlaaShaheen2/Math-Magicians"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Alaa Project
-          </a>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Heading </h1>
     </div>
   );
 }
