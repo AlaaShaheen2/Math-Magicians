72741835076b8a2defd94a0d2437e8751a384f4b
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-09-29 00:50:20 +0300

    setup router & make pages

---
+++ src/components/pages/Home.js
@@ -0,0 +1,20 @@
+const HomePage = () => (
+  <main className="home">
+    <h1 className="title">Welcome to Math Magicians App</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+      asperiores molestias iste facilis nihil, sint ut vero illo iusto quae
+      ullam optio voluptatum, ab odit, magni possimus obcaecati esse explicabo!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis totam,
+      perspiciatis voluptate voluptatibus impedit fuga ab hic facere provident,
+      sed nihil asperiores fugiat, iure maxime eum voluptatem natus corporis
+      quasi?
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+      asperiores molestias iste facilis nihil, sint ut vero illo iusto quae
+      ullam optio voluptatum, ab odit, magni possimus obcaecati esse explicabo!
+    </p>
+  </main>
+);
+export default HomePage;
---
+++ src/components/pages/Quote.js
@@ -0,0 +1,9 @@
+const QuotePage = () => (
+  <main className="quote-page">
+    <p>
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding. - William Paul Thurston
+    </p>
+  </main>
+);
+export default QuotePage;
